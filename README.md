b30bb25e5d355843782040b7f870aaa6ec803f57
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-09-06 15:02:53 -0300

    adding error alert

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# imc-calculator
+# bmi-calculator
--- script.js
+++ script.js
@@ -6,11 +6,30 @@ const modalWrapper = document.querySelector('.modal-wrapper');
 const modalMessage = document.querySelector('.modal .title span');
 const modalButton = document.querySelector('.modal button.close');
 
+const alertError = document.querySelector('.alert-error');
+
+inputWeight.oninput = () => {
+    alertError.classList.remove('open');
+}
+
+inputHeight.oninput = () => {
+    alertError.classList.remove('open');
+}
+
 form.onsubmit = event => {
     event.preventDefault();
 
     const weight = inputWeight.value;
     const height = inputHeight.value;
+
+    const showError = notNumber(weight) || notNumber(height);
+
+    if (showError) {
+        alertError.classList.add('open');
+        return;
+    }
+
+    alertError.classList.remove('open');
 
     const result = calculateBMI(weight, height);
     const message = `Your BMI is ${result}`;
@@ -23,7 +42,18 @@ modalButton.onclick = () => {
     modalWrapper.classList.remove('open');
 }
 
+function notNumber(value) {
+    return isNaN(value) || value === '' || value === null;
+}
 
 function calculateBMI(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2);
+}
+
+window.addEventListener('keydown', handleKeydown);
+
+function handleKeydown(event) {
+    if (event.key === 'Escape') {
+        modalWrapper.classList.remove('open');
+    }
 }
